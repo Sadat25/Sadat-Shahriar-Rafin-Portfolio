b79ca5c2fc1bb46983d138da3c33910284a132a6
Sadat Shahriar Rafin <sadatshahriarrafin@gmail.com>
2025-12-20 18:26:47 +0600

    footer

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -45,9 +45,7 @@ const Testimonials = () => {
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-      >
-        What Peiple Say
-      </motion.h2>
+      >What People Say</motion.h2>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 w-full max-w-6xl'>
         {testimonials.map((t, i) => (
           <motion.div
