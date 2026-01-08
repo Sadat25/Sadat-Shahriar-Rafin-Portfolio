1c117c1d30588c5b4f3672285f40c726e052539b
Sadat Shahriar Rafin <sadatshahriarrafin@gmail.com>
2026-01-08 21:03:58 +0600

    Fix formatting issues in ParticlesBg component

--- src/components/ParticlesBg.jsx
+++ src/components/ParticlesBg.jsx
@@ -7,22 +7,22 @@ const ParticlesBg = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    let particles = [];
+    let particles = [];    
     const particlesCount = 50;
-    const colors = ['rgba(255,255,255,0.7)'];
+    const colors = ['rgba(255,255,255,0.7)'];         
     class Particle {
       constructor() {
         this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.y = Math.random() * canvas.height;         
         this.radius = Math.random() * 2 + 1;
         this.color = colors[Math.floor(Math.random() * colors.length)];
-        this.speedX = (Math.random() - 0.5) * 0.5;
+        this.speedX = (Math.random() - 0.5) * 0.5;           
         this.speedY = (Math.random() - 0.5) * 0.5;
       }
 
       drow() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);           
         ctx.shadowBlur = 10;
         ctx.shadowColor = this.color;
         ctx.fillStyle = this.color;
